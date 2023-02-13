7bcdb590cc553925140a7429a25b637b9a4ec2d5
Lee Crawford <lee.crawford@gmail.com>
2023-02-13 07:38:15 -0800

    Displaying configuration on system pagE

--- fe/src/routes/system/+page.svelte
+++ fe/src/routes/system/+page.svelte
@@ -1,52 +1,60 @@
 <script>
     import {onMount} from "svelte";
-    import Alert from "../../components/Alert.svelte";
     import {maybeError, withCommas} from "$lib/Utils.js";
-    import SECReport from "$lib/SECReport.js";
     import Server from "$lib/Server.js";
+    import PropertyTable from "../advisors/[slug]/PropertyTable.svelte";
 
+    let configuration = { "Loaded": false };
     let error = null;
     let loading = false;
     let elapsed = -1;
     let alert = null;
 
-    async function reloadFirms () {
-        const start = Date.now ();
+    async function invoke (op) {
         try {
             loading = true;
             error = null;
             alert = null;
-            const query = "mutation { res: reloadFirms { count }  }";
-            const res = await Server._gql (query);
-            elapsed = Date.now() - start;
-            alert = "Reloaded " + withCommas (res.count) + " firms in " + withCommas (elapsed) + "ms."
-        } catch (e) {
-            error = maybeError (e);
-        } finally {
-            loading = false;
+            await op ();
         }
-        return;
-    }
-    async function reloadIndividuals () {
-        const start = Date.now ();
-        try {
-            loading = true;
-            error = null;
-            alert = null;
-            const query = "mutation { res: reloadIndividuals { count }  }";
-            const res = await Server._gql (query);
-            elapsed = Date.now() - start;
-            alert = "Reloaded " + withCommas (res.count) + " individuals in " + withCommas (elapsed) + "ms."
-        } catch (e) {
-            error = maybeError (e);
+        catch (e) {
+            console.log (e);
+            error = maybeError(e);
         } finally {
             loading = false;
         }
         return;
     }
 
+    async function loadSystemConfiguration () {
+        await invoke (async () => {
+            const query = "query { res: getSystemConfiguration { env filesystem } }";
+            configuration = await Server._gql (query);
+        });
+    }
+
+    async function reloadFirms () {
+        await invoke (async () => {
+            const start = Date.now ();
+            const query = "mutation { res: reloadFirms { count }  }";
+            const res = await Server._gql (query);
+            elapsed = Date.now() - start;
+            alert = "Reloaded " + withCommas (res.count) + " firms in " + withCommas (elapsed) + "ms."
+        });
+    }
+
+    async function reloadIndividuals () {
+        await invoke (async () => {
+            const start = Date.now ();
+            const query = "mutation { res: reloadIndividuals { count }  }";
+            const res = await Server._gql (query);
+            elapsed = Date.now() - start;
+            alert = "Reloaded " + withCommas (res.count) + " individuals in " + withCommas (elapsed) + "ms."
+        });
+    }
+
     onMount (async () => {
-        // report = await loadStats ()
+        await loadSystemConfiguration();
     });
 </script>
 
@@ -72,6 +80,12 @@
     </sl-alert>
     <br/>
 {/if}
+
+<h2>Configuration</h2>
+
+<PropertyTable props={configuration} />
+
+<h2>Updating</h2>
 
 <sl-button variant="default" on:click={reloadFirms}>
     <sl-icon name="arrow-clockwise"></sl-icon>&nbsp;Reload Firms
--- sec/be/src/main/kotlin/graphql/system/SystemFetchers.kt
+++ sec/be/src/main/kotlin/graphql/system/SystemFetchers.kt
@@ -1,12 +1,21 @@
 package graphql.system
 
+import com.example.config.Config
+import com.example.config.LocalFilesystem
+import com.example.config.config
 import com.example.util.ILoggable
+import com.github.salomonbrys.kodein.instance
 import graphql.Wireable
 import graphql.schema.DataFetcher
 import graphql.schema.idl.RuntimeWiring
 import graphql.schema.idl.TypeRuntimeWiring
 
 data class GraphQLVoid (val success: Boolean =  true)
+
+data class SystemConfiguration (
+    val env: String,
+    val filesystem: String
+)
 
 /**
  *
@@ -22,8 +31,16 @@ object SystemFetchers : ILoggable, Wireable {
                     .dataFetcher ("noOp", noOp)
                     .dataFetcher ("throwException", throwException)
                     .dataFetcher ("delay", delay)
+                    .dataFetcher ("getSystemConfiguration", getConfig)
                 )
     }
+
+    private val getConfig: DataFetcher<SystemConfiguration>
+        get () = DataFetcher { dfe ->
+            val conf: Config = config.instance ()
+            val filesystem = conf.get (LocalFilesystem::class.java)
+            SystemConfiguration (config.instance("env"), filesystem!!.resourceRoot)
+        }
 
     private val delay: DataFetcher<Boolean>
         get () = DataFetcher { dfe ->
